529e287e46d6cb74d7c6d63c3da7b4cc62e3b1f7
seungwookim <osabero@naver.com>
2023-10-30 17:20:28 +0900

    ADD/Modal/gesture 추가

--- dev/FE/src/components/model/AudioUploadModal.tsx
+++ dev/FE/src/components/model/AudioUploadModal.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
-import { useSpring, animated } from '@react-spring/web';
+import { useSpring, animated, config } from '@react-spring/web';
 import { useDrag } from '@use-gesture/react';
 import { LargeButton } from '../common';
 
@@ -10,18 +10,17 @@ const BackGround = styled.div`
   left: 0;
   width: 100vw;
   height: 100vh;
-  background: rgba(0, 0, 0, 0.7);
+  background: rgba(0, 0, 0, 0);
   z-index: 1000;
 `;
 
 const Modal = styled.div`
-  position: fixed;
-  bottom: 0;
+  box-sizing: border-box;
+  width: 100vw;
   left: 0;
   right: 0;
-  height: 80vh;
+  height: calc(80vh + 120px);
   background-color: white;
-  /* padding: 20px; */
   box-shadow: 0 -5px 15px rgba(0, 0, 0, 0.1);
   border-radius: 16px 16px 0 0;
   z-index: 1500;
@@ -35,7 +34,7 @@ const Handle = styled.div`
   margin: 36px auto 0 auto;
 `;
 const TitleWrapper = styled.div`
-  width: 92vw;
+  width: 88vw;
   height: fit-content;
   text-align: center;
   margin: 24px auto;
@@ -70,6 +69,7 @@ const ListWrapper = styled.ul`
   height: 50vh;
   overflow-y: auto;
   margin: 0 auto;
+  padding: 0;
 `;
 
 const ListItem = styled.li`
@@ -85,10 +85,66 @@ interface AudioFile {
 const AudioUploadModal: React.FC<{
   onClose: () => void;
 }> = ({ onClose }) => {
-  const [{ x, y }, api] = useSpring(() => ({ x: 0, y: 0 }));
-  const bind = useDrag(({ down, movement: [mx, my] }) => {
-    api.start({ x: down ? mx : 0, y: down ? my : 0, immediate: down });
+  const HEIGHT = window.innerHeight * 0.8;
+  const [{ y }, api] = useSpring(() => ({ y: HEIGHT }));
+
+  const open = () => {
+    api.start({
+      y: 0,
+      immediate: false,
+      config: config.stiff,
+    });
+  };
+
+  useEffect(() => {
+    open();
   });
+
+  const close = (velocity = 0) => {
+    api.start({
+      y: HEIGHT,
+      immediate: false,
+      config: { ...config.stiff, velocity },
+    });
+    onClose();
+  };
+
+  const bind = useDrag(
+    ({ last, velocity: [, vy], direction: [, dy], offset: [, oy], cancel }) => {
+      // if the user drags up passed a threshold, then we cancel
+      // the drag so that the sheet resets to its open position
+      if (oy < -70) cancel();
+
+      // when the user releases the sheet, we check whether it passed
+      // the threshold for it to close, or if we reset it to its open positino
+      if (last) {
+        if (oy > HEIGHT * 0.5 || (vy > 0.5 && dy > 0)) {
+          close();
+        } else {
+          open();
+        }
+      }
+      // when the user keeps dragging, we just move the sheet according to
+      // the cursor position
+      else api.start({ y: oy, immediate: true });
+    },
+    {
+      from: () => [0, y.get()],
+      filterTaps: true,
+      bounds: { top: 0 },
+      rubberband: true,
+    },
+  );
+
+  const display = y.to((py) => (py < HEIGHT ? 'block' : 'none'));
+
+  const bgStyle = {
+    backgroundColor: y.to(
+      [0, HEIGHT],
+      ['rgba(0, 0, 0, 0.7)', 'rgba(0, 0, 0, 0.3)'],
+      'clamp',
+    ),
+  };
 
   const [audioFiles, setAudioFiles] = useState<AudioFile[]>([]);
 
@@ -110,23 +166,24 @@ const AudioUploadModal: React.FC<{
     );
   };
 
+  const AnimatedBackground = animated(BackGround);
+  const AnimatedModal = animated(Modal);
+
   return (
-    <BackGround>
-      <Modal>
-        <animated.div
-          {...bind()}
-          style={{
-            x,
-            y,
-            width: '200px',
-            height: '50px',
-            backgroundColor: '#000',
-          }}
-        />
-        <Handle onClick={onClose} />
+    <AnimatedBackground style={bgStyle}>
+      <AnimatedModal
+        {...bind()}
+        style={{
+          display,
+          bottom: `calc(-100px)`,
+          y,
+          position: 'fixed',
+          touchAction: 'none',
+        }}
+      >
+        <Handle />
         <TitleWrapper>
           <Title>음성 업로드</Title>
-          {/* <button onClick={onClose}>닫기</button> */}
           <Label htmlFor="AudioUploadInput">
             <Input
               id="AudioUploadInput"
@@ -150,8 +207,8 @@ const AudioUploadModal: React.FC<{
           ))}
         </ListWrapper>
         <LargeButton content="저장" />
-      </Modal>
-    </BackGround>
+      </AnimatedModal>
+    </AnimatedBackground>
   );
 };
 
