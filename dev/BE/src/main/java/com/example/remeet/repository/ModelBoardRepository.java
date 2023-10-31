639c05dba9827cc593242a5d39be5272153e20e3
이승준 <ja132824@naver.com>
2023-10-31 16:56:20 +0900

    :sparkles:_최근대화영상 조회 API 생성_(#290)

---
+++ dev/BE/src/main/java/com/example/remeet/repository/ModelBoardRepository.java
@@ -0,0 +1,10 @@
+package com.example.remeet.repository;
+
+import com.example.remeet.entity.ModelBoardEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ModelBoardRepository extends JpaRepository<ModelBoardEntity, Integer> {
+
+}
