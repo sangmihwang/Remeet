401ed5b269c3a7178ce0a5afdc6c07e463edd515
이승준 <ja132824@naver.com>
2023-11-03 14:26:11 +0900

    :bug:_talkingcontroller 수정_(#286)

--- dev/BE/src/main/java/com/example/remeet/controller/TalkingController.java
+++ dev/BE/src/main/java/com/example/remeet/controller/TalkingController.java
@@ -18,7 +18,6 @@ import org.springframework.web.multipart.MultipartFile;
 @RestController
 @Slf4j
 public class TalkingController {
-    private final TalkingService talkingService;
     private final GPTService gptService;
     private final TTSService ttsService;
     private final FlaskService flaskService;
@@ -26,8 +25,7 @@ public class TalkingController {
     @PostMapping("stt/{voiceId}")
     public ResponseEntity<STTResponseDto> upload(@RequestPart(value = "file") MultipartFile multipartFile, @PathVariable("voiceId") String voiceId) throws Exception {
         String wavPath = flaskService.callFlaskByMultipartFile(multipartFile, "stt");
-        String msg = talkingService.callFlaskApi(wavPath).getText();
-        String answer = gptService.callFlaskApi(msg).getText();
+        String answer = gptService.callFlaskApi(wavPath).getText();
         STTResponseDto audioPath = ttsService.callFlaskApi(answer, voiceId);
         return new ResponseEntity<STTResponseDto>(audioPath, HttpStatus.OK);
     }
