fc84558432c9e7bdcf12cc9bc4ec448231824593
이승준 <ja132824@naver.com>
2023-11-08 16:14:12 +0900

    Update dev/FLASK/conversation.py

--- dev/FLASK/conversation.py
+++ dev/FLASK/conversation.py
@@ -493,9 +493,9 @@ def upload_avatar():
         data=file.read(),  # 파일의 내용을 읽어서 줘야함
         headers={"Content-Type": "image/jpeg", "x-api-key": x_api_key},
     )
-    result = json.loads(resp)
-
-    return jsonify({"result" : result["data"]["talking_photo_id"]}), 200
+    
+    app.logger.info("CREATE_AVATAR_ID API Response result : ", resp.status_code)
+    return jsonify({"result" : resp.json()["data"]["talking_photo_id"]}), 200
 
 # 기본 영상 생성 API
 # 기본 영상 생성 API
