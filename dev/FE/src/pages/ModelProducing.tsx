255fc3a702b15172cf2059f27fcafe301647eca3
sangmihwang <01071184366@gmail.com>
2023-11-10 17:58:17 +0900

    :sparkles:_모델 생성 대기화면 구현 중_(#309)

--- dev/FE/src/pages/ModelProducing.tsx
+++ dev/FE/src/pages/ModelProducing.tsx
@@ -1,15 +1,18 @@
-import styled from 'styled-components';
-import { useNavigate } from 'react-router-dom';
+import styled, { keyframes } from 'styled-components';
+import { useQuery } from '@tanstack/react-query';
+import { useNavigate, useParams } from 'react-router-dom';
 import PageHeader from '@/components/navbar/PageHeader';
 import BottomNavigation from '@/components/navbar/BottomNavigation';
-import { Image, LargeButton } from '@/components/common';
+import { Image, SmallButton } from '@/components/common';
 import useAuth from '@/hooks/useAuth';
+import { ModelInformation } from '@/types/peopleList';
+import { getPeopleInfo } from '@/api/peoplelist';
 
 const HeaderBackGround = styled.div`
   top: 0;
   position: absolute;
   width: 100%;
-  height: 24rem;
+  height: 20rem;
   background-color: var(--primary-color);
   z-index: -200;
 `;
@@ -20,30 +23,81 @@ const TitleWrapper = styled.div`
 `;
 
 const Title = styled.div`
-  font-size: 1.825rem;
+  font-size: 1.6rem;
   font-weight: 600;
-  width: fit-content;
   margin: 1rem auto;
+  width: fit-content;
 `;
 const ButtonWrapper = styled.div`
-  margin: 1rem;
+  margin: 1rem auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 const ImageWrapper = styled.div`
-  margin: 2rem auto 0 auto;
   width: fit-content;
+  margin: 5rem auto 0;
+`;
+const ServiceName = styled.div`
+  color: var(--primary-color);
+  font-size: 1.6rem;
+  font-weight: 600;
+  margin: 0.5rem auto;
+  width: fit-content;
+`;
+const ProducingAlert = styled.div`
+  margin: 0.3rem auto;
+  width: 86vw;
+  height: 3.2rem;
+  border-radius: 100px;
+  background-color: #f6f6f6;
+  color: var(--primary-color);
+  border: 0;
+  font-weight: 600;
+  font-size: 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+const fillAnimation = keyframes`
+  from { width: 0%; }
+  to { width: 80%; }
+`;
+const ProgressBar = styled.div`
+  height: 10%;
+  background-color: var(--primary-color);
+  width: 0%;
+  border-radius: 5px;
+  margin-top: 1.5rem;
+  margin-left: 10%;
+  animation: ${fillAnimation} 15s ease-in-out forwards;
+`;
+const ProgressWrapper = styled.div`
+  margin: 1rem auto;
+  width: 90%;
+  height: 12rem;
+  border-radius: 20px;
+  background-color: #f6f6f6;
+  overflow: hidden;
 `;
 
 const ModelProducing = () => {
   const { userInfo } = useAuth();
   const navigate = useNavigate();
+  const { modelNo } = useParams<{ modelNo?: string }>();
   const headerContent = {
     left: 'Back',
     title: 'Re:meet',
     right: '',
   };
+  const { data: modelInfomation } = useQuery<ModelInformation | undefined>(
+    ['getModelInfo'],
+    () => getPeopleInfo(Number(modelNo)),
+  );
+  console.log(userInfo?.imagePath);
   const handleTalkStart = () => {
-    if (userInfo && userInfo.modelNo) {
-      navigate(`/talk/${userInfo.modelNo}`);
+    if (modelNo) {
+      navigate(`/talk/${modelNo}`);
     } else {
       console.error('해당 번호 모델 없음');
     }
@@ -54,13 +108,32 @@ const ModelProducing = () => {
       <TitleWrapper>
         <PageHeader content={headerContent} type={2} />
       </TitleWrapper>
+      <ProgressWrapper>
+        <ProducingAlert>모델을 제작 중입니다</ProducingAlert>
+        <ServiceName>Re:meet</ServiceName>
+        <ProgressBar />
+      </ProgressWrapper>
       <ImageWrapper>
-        <Image src={userInfo?.imagePath as string} />
+        <Image
+          src={
+            modelInfomation ? modelInfomation.imagePath : '/dummy/갱얼쥐.jpg'
+          }
+        />
       </ImageWrapper>
-      <Title>{userInfo?.userName}</Title>
+      <Title>{modelInfomation && modelInfomation.modelName}</Title>
       <ButtonWrapper>
-        <LargeButton content="대화 시작" onClick={handleTalkStart} />
+        <SmallButton
+          type={1}
+          text="보이스톡"
+          onClick={() => handleTalkStart()}
+        />
+        <SmallButton
+          type={1}
+          text="페이스톡"
+          onClick={() => handleTalkStart()}
+        />
       </ButtonWrapper>
+
       <BottomNavigation />
     </div>
   );
