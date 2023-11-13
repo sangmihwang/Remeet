51dc5c07ccd08ff599bae98ba2d4ed9a18727c47
seungwookim <osabero@naver.com>
2023-11-13 11:01:53 +0900

    MOD/build되게 수정

--- dev/FE/src/pages/ModelProducing.tsx
+++ dev/FE/src/pages/ModelProducing.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import styled, { keyframes } from 'styled-components';
 import { useQuery } from '@tanstack/react-query';
 import { useNavigate, useParams } from 'react-router-dom';
