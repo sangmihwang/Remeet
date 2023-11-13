f4f33b7a7747122ed5833687f66921f1230e7c70
sangmihwang <01071184366@gmail.com>
2023-11-13 15:32:58 +0900

    :sparkles:_랜딩페이지 구현_(#412)

--- dev/FE/src/pages/MainPage.tsx
+++ dev/FE/src/pages/MainPage.tsx
@@ -1,5 +1,6 @@
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import styled from 'styled-components';
+import { keyframes, styled } from 'styled-components';
 
 const Wrapper = styled.div`
   background-color: var(--primary-color);
@@ -11,7 +12,7 @@ const Title = styled.div`
   top: 17vh;
   left: 10vw;
   color: #fff;
-  font-size: 2.125rem;
+  font-size: 2.5rem;
   font-weight: 700;
   width: fit-content;
   height: fit-content;
@@ -20,13 +21,22 @@ const Title = styled.div`
 const ButtonWrapper = styled.div`
   box-sizing: border-box;
   position: absolute;
-  bottom: 17vh;
+  bottom: 20vh;
   width: 100vw;
   padding: 0 10vw;
   display: flex;
   justify-content: space-between;
 `;
-
+const fadeInUp = keyframes`
+  from {
+    opacity: 0;
+    transform: translateY(20px);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+`;
 const Button = styled.button`
   box-sizing: border-box;
   min-width: 35vw;
@@ -35,13 +45,34 @@ const Button = styled.button`
   border: 0;
   border-radius: 10px;
   background-color: #fff;
-  font-size: 1.125rem;
-  font-weight: 600;
+  font-size: 1rem;
+  font-weight: 700;
   color: var(--primary-color);
+  animation: ${fadeInUp} 1.5s ease-out forwards;
+`;
+const ImageWrapper = styled.div`
+  position: absolute;
+  top: 37vh;
+  left: 10vw;
+  width: 80vw;
+`;
+const StyledImage = styled.img`
+  width: 100%;
+  opacity: 0.85;
+  animation: ${fadeInUp} 2s ease-out forwards;
 `;
 
 const MainPage = () => {
+  const [showLanding, setShowLanding] = useState(true);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowLanding(false);
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, []);
 
   const handleGoLogin = () => {
     navigate('/login');
@@ -49,13 +80,25 @@ const MainPage = () => {
   const handleGoSignUp = () => {
     navigate('/signup');
   };
+
   return (
     <Wrapper>
-      <Title>Re:meet</Title>
-      <ButtonWrapper>
-        <Button onClick={handleGoLogin}>로그인</Button>
-        <Button onClick={handleGoSignUp}>회원가입</Button>
-      </ButtonWrapper>
+      {showLanding ? (
+        <>
+          <Title>Re:meet</Title>
+          <ImageWrapper>
+            <StyledImage src="/LandingImage.svg" alt="Landing Image" />
+          </ImageWrapper>
+        </>
+      ) : (
+        <>
+          <Title>Re:meet</Title>
+          <ButtonWrapper>
+            <Button onClick={handleGoLogin}>로그인</Button>
+            <Button onClick={handleGoSignUp}>회원가입</Button>
+          </ButtonWrapper>
+        </>
+      )}
     </Wrapper>
   );
 };
