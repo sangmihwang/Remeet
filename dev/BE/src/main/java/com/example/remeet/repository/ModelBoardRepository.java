2c07f85a38f409cfaacfad605ebbc444aa51d14a
이승준 <ja132824@naver.com>
2023-11-16 14:55:41 +0900

    :art:_conversation 수정_(#492)

--- dev/BE/src/main/java/com/example/remeet/repository/ModelBoardRepository.java
+++ dev/BE/src/main/java/com/example/remeet/repository/ModelBoardRepository.java
@@ -14,21 +14,21 @@ import java.util.Optional;
 @Repository
 public interface ModelBoardRepository extends JpaRepository<ModelBoardEntity, Integer> {
     @Query("SELECT new com.example.remeet.dto.ModelBoardDto(" +
-            "m.modelNo, m.modelName, m.imagePath, m.eleVoiceId, m.heyVoiceId, m.commonHoloPath) " +
+            "m.modelNo, m.modelName, m.imagePath, m.eleVoiceId, m.heyVoiceId, m.movingVideoPath) " +
             "FROM ModelBoardEntity m " +
             "WHERE m.userNo.userNo = :userNo " +
             "ORDER BY m.conversationCount DESC")
     List<ModelBoardDto> findTopModelsByUserNo(Integer userNo, Pageable pageable);
 
     @Query("SELECT new com.example.remeet.dto.ModelBoardDto(" +
-            "m.modelNo, m.modelName, m.imagePath, m.eleVoiceId, m.heyVoiceId, m.commonHoloPath) " +
+            "m.modelNo, m.modelName, m.imagePath, m.eleVoiceId, m.heyVoiceId, m.movingVideoPath) " +
             "FROM ModelBoardEntity m " +
             "WHERE m.userNo.userNo = :userNo " +
             "ORDER BY m.latestConversationTime DESC")
     List<ModelBoardDto> findMostRecentByUserNo(Integer userNo, Pageable pageable);
 
     @Query("SELECT new com.example.remeet.dto.ModelBoardDto(" +
-            "m.modelNo, m.modelName, m.imagePath, m.eleVoiceId, m.heyVoiceId, m.commonHoloPath) " +
+            "m.modelNo, m.modelName, m.imagePath, m.eleVoiceId, m.heyVoiceId, m.movingVideoPath) " +
             "FROM ModelBoardEntity m " +
             "WHERE m.userNo.userNo = :userNo ")
     List<ModelBoardDto> findByUserNo(Integer userNo);
